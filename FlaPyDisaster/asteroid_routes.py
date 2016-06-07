901847f3dcee8f28fa6d85cd8e3d55aad22b6bde
ungawatkt <ungawa555tkt@gmail.com>
2016-06-07 13:20:16 -0400

    adding form inputs to asteroid page

--- FlaPyDisaster/asteroid_routes.py
+++ FlaPyDisaster/asteroid_routes.py
@@ -13,3 +13,14 @@ def asteroid_function_form():
     exmath.hello()
     astermath.hello()
     return redirect(url_for('asteroid_page'))
+
+@app.route('/asteroid_input_params_function', methods = ['POST'])
+def asteroid_input_params_form():
+    diameter_m = request.form['diameter_m']
+    angle_deg = request.form['angle_deg']
+    velocity_kms = request.form['velocity_kms']
+    density_kgm3 = request.form['density_kgm3']
+    target_density_kgm3 = request.form['target_density_kgm3']
+
+    print('asteroid_input_params_form ' + diameter_m)
+    return redirect(url_for('asteroid_page'))

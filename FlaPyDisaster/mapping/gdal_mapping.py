fb0d610ce4692a3e4b4e6ecf58f0c343c1ef8f29
ungawatkt <ungawa555tkt@gmail.com>
2016-09-07 22:55:47 -0400

    clean up gdal mapping function

--- FlaPyDisaster/mapping/gdal_mapping.py
+++ FlaPyDisaster/mapping/gdal_mapping.py
@@ -11,13 +11,20 @@ def list_to_raster(val_list, file_uri, overwrite=False):
     cols = val_array.shape[1]
     rows = val_array.shape[0]
 
-    driver = gdal.GetDriverByName('MEM')
-    png_driver = gdal.GetDriverByName('PNG')
-    temp_raster = driver.Create('', cols, rows, 1, gdal.GDT_Byte)
+    # Create memory raster
+    mem_driver = gdal.GetDriverByName('MEM')
+    mem_raster = mem_driver.Create('', cols, rows, 1, gdal.GDT_Byte)
 
-    outband = temp_raster.GetRasterBand(1)
+    # Write array to memory raster
+    outband = mem_raster.GetRasterBand(1)
     outband.WriteArray(val_array)
-    out_raster = png_driver.CreateCopy(file_uri, temp_raster, 0)
+
+    # make physical png raster from memory raster
+    png_driver = gdal.GetDriverByName('PNG')
+    out_raster = png_driver.CreateCopy(file_uri, mem_raster, 0)
+
+    # Explicitly close rasters and bands, might not be necessary
+    mem_raster.FlushCache()
     outband.FlushCache()
     out_raster.FlushCache()
 
